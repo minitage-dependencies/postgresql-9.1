be8e9242a209af1abd62f95d7a6941d125dec56a
makina <makina@APEM3.(none)>
2009-11-24 19:53:45 +0100

    win32port

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -1,5 +1,26 @@
+import sys
+import shutil
 import os
 
 def getpostgresqlenv(options,buildout):
     os.environ['EXTRA_LDAP_LIBS'] = os.environ['LDFLAGS']
+    
+    
+def getpostgresqlenv(options,buildout):
+    os.environ['EXTRA_LDAP_LIBS'] = os.environ['LDFLAGS']
+    
+def p(o, b):
+    if 'win' in sys.platform:
+        dest = o['location']
+        lib = os.path.join(dest, 'lib')
+        bin = os.path.join(dest, 'bin')
+        for dll in [libdll 
+                    for libdll in os.listdir(lib) 
+                    if libdll.endswith('dll')]:
+            orig, ldest = os.path.join(lib, dll), os.path.join(bin, dll)
+            if os.path.exists(ldest):
+                os.remove(ldest)
+            shutil.copy2(orig, ldest)
+
+    
 # vim:set ts=4 sts=4 et  :
